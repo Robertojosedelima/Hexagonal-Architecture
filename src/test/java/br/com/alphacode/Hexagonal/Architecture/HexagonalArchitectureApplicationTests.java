eb0f966d636990beead870e0176755f40a54d5d5
Roberto lima <drop1108@gmail.com>
2026-05-17 20:51:04 -0300

    Fist commit.

---
+++ src/test/java/br/com/alphacode/Hexagonal/Architecture/HexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alphacode.Hexagonal.Architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
